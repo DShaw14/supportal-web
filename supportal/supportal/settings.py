152c8f8030a673791de2af50f92106c4752af6ed
David <david14shaw@gmail.com>
2021-01-26 23:58:10 -0500

    Update settings.py

--- supportal/supportal/settings.py
+++ supportal/supportal/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y^$xaawe^n)-l%j0rf@@#x2bfde@^i)-5-0s5k(8sf3czu6@8s'
+SECRET_KEY = 'xxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
